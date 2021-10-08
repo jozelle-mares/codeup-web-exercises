0622dbdd96506f6490aaf78f363e863d2d3e768c
Jozelle Mares <jozelle.mares@gmail.com>
2021-10-08 16:50:28 -0500

    completed js exercises

---
+++ js/external.js
@@ -0,0 +1,50 @@
+console.log("Hello from external Javascript.")
+
+alert = ("Welcome to my website!");
+
+var favoriteColor = prompt("Whats your favorite color?");
+alert ("Great,my favorite color is " +  favoriteColor  + " too!");
+
+alert ("Movies will cost $3 per day they are rented.");
+
+
+var littleMermaid = parseFloat(prompt("How many days have you rented the Little Mermaid?"));
+
+var brotherBear = parseFloat(prompt("How many days have you rented Brother Bear?"));
+
+var herCules = parseFloat(prompt("How many days have you rented Hercules?"));
+
+var moviePrice = (littleMermaid + brotherBear + herCules) * 3;
+
+alert ("You have paid $" + moviePrice + " for your movies.");
+
+var gooGle = 400
+var amaZon = 380
+var faceBook = 350
+
+var hoursGoogle = parseFloat(prompt("How many hours did you work this week at Google?"));
+
+var googPay = parseFloat(prompt( "How much do you get paid hourly at Google?"));
+
+var hoursAmazon = parseFloat(prompt("How many hours did you work this week at Amazon?"));
+
+var amaPay = parseFloat(prompt( "How much do you get paid hourly at Amazon?"));
+
+var hoursFacebook = parseFloat(prompt("How many hours did you work this week at Facebook?"));
+
+var facePay = parseFloat(prompt( "How much do you get paid hourly at Facebook?"));
+
+var totalPay = (googPay * hoursGoogle) + (amaPay * hoursAmazon) + (facePay * hoursFacebook);
+
+alert("Way to go, you made $" + totalPay + " this week!")
+
+var classRoom = confirm("Is there room in the Intro to KPOP class?");
+
+var yourSchedule = confirm("Do you have classes on Thursday's at 1pm?");
+
+alert("Student is able to register for class:" + (classRoom && yourSchedule));
+
+var primeMem = ("Do you have a prime membership?");
+
+var itemAmount
+
--- js/js-with-html-lec.js
+++ js/js-with-html-lec.js
@@ -12,9 +12,9 @@ var returnOfConfirm = confirm("Got any plans for this three day weekend?")
 console.log(returnOfConfirm);
 
 var favoriteNuMetalBand = prompt("What's your favorite new metal band");
-console.log("Ha ha, you like" + favoriteNuMetalBand + "!")
+console.log("Ha ha, you like " +  favoriteNuMetalBand + "!")
 
 var leastFavoriteSoda = prompt("What is your least favorite soda?");
 var favoriteSoda = prompt("What's your favorite soda?");
 
-alert("Hello fellow teens, my name is " + leastFavoriteSoda + "" + favoriteSoda + " and I choose hugs not drugs.");
+alert("Hello fellow teens, my name is " + leastFavoriteSoda + " " + favoriteSoda + " and I choose hugs not drugs.");
