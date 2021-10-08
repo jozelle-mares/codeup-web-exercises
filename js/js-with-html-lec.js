dfe8ffafe71780db73a966c947fb7c434d21e217
Jozelle Mares <jozelle.mares@gmail.com>
2021-10-08 10:49:11 -0500

    completed the js with html lec

---
+++ js/js-with-html-lec.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var jeff = "Jeff";
+console.log(jeff);
+
+console.log("Hello! I'm in my own file now!");
+
+var returnOfAlert = alert("TGIF! Thank Goodness I'm Fred!");
+console.log(returnOfAlert);
+
+var returnOfConfirm = confirm("Got any plans for this three day weekend?")
+console.log(returnOfConfirm);
+
+var favoriteNuMetalBand = prompt("What's your favorite new metal band");
+console.log("Ha ha, you like" + favoriteNuMetalBand + "!")
+
+var leastFavoriteSoda = prompt("What is your least favorite soda?");
+var favoriteSoda = prompt("What's your favorite soda?");
+
+alert("Hello fellow teens, my name is " + leastFavoriteSoda + "" + favoriteSoda + " and I choose hugs not drugs.");
