ed776c8d47d95499c1cc48c2a48cc0bf65557da6
Jozelle Mares <jozelle.mares@gmail.com>
2021-10-19 09:26:44 -0500

    morning warmup

--- js/morning-warmups.js
+++ js/morning-warmups.js
@@ -21,14 +21,37 @@
     console.log(action);
 
 // October 15th
-function typeCheck(input) {
-    if (typeof input === 'string'){ ;
-        return 'Haha, it\'s a string of a string';
-    } else {
-        return typeof input;
+    function typeCheck(input) {
+        if (typeof input === 'string') {
+            ;
+            return 'Haha, it\'s a string of a string';
+        } else {
+            return typeof input;
+        }
+
     }
 
-}
+// October 19th
+//Write a program that console.logs the numbers from 1 to 100.
+// But for multiples of three console.log “Fizz” instead of the number
+// and for the multiples of five console.log “Buzz”. For numbers which are
+// multiples of both three and five console.log “FizzBuzz”.
 
+    for (var x = 1; x <= 100; x++) {
+        if (x % 3 === 0) {
+            console.log("Fizz")
+        } else {
+            if (x % 5 === 0) {
+                console.log("Buzz")
+            } else {
+                if (x % 3 === 0 && x & 5 === 0) {
+                    console.log("FizzBuzz")
+                } else {
+                    console.log(x)
+                }
+            }
+
+        }
+    }
 
 })();
