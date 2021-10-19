bd6182296cf576a98999f43c73e91be38ae130d3
Jozelle Mares <jozelle.mares@gmail.com>
2021-10-19 08:49:43 -0500

    updated while exercise

--- js/while.js
+++ js/while.js
@@ -4,3 +4,18 @@ while (count < 16) {
     console.log(count);
     count +=( 1 * count);
 }
+function iceCream() {
+    var allCones = Math.floor(Math.random() * 50) + 50;
+    console.log("Cones to sell " + allCones);
+
+    do {
+        var bought = Math.floor(Math.random() * 5) + 5;
+        console.log("Purchased: " + bought);
+        allCones -= bought
+    } else
+    {
+        console.log("Cannot sell " + bought + " I only have" + allCones);
+    }
+    while (allCones > 0) ;
+    console.log("All cones were sold!")
+}
